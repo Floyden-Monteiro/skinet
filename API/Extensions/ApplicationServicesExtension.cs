259067b960a6b1eadb5cef302819742e4ee9d50f
Floyden-Monteiro <floydensocialmedia@gmail.com>
2025-01-22 15:36:31 +0530

    Add: implement navigation bar component, update CORS policy, and refactor asset paths in Angular configuration

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -43,7 +43,7 @@ public static class ApplicationServicesExtension
         {
             opt.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
             });
         });
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, NavBarComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skinet';
+  products: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:5001/api/products?pageSize=50").subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error => console.error(error),
+      complete: () => console.log("complete")
+    });
+  }
 }
