ceff3859673e10bde623f0968911ea60994615d7
Floyden-Monteiro <floydensocialmedia@gmail.com>
2025-01-28 18:46:39 +0530

    Add: implement lazy loading for shop and product details components in routing ✨

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details.component';
+
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'shop', component: ShopComponent },
-    { path: 'shop/:id', component: ProductDetailsComponent },
+    { path: '', component: HomeComponent }, 
+    
+
+    {
+        path: 'shop',
+        loadComponent: () => import('./shop/shop.component').then(m => m.ShopComponent)
+    },
+
+
+    {
+        path: 'shop/:id',
+        loadComponent: () => import('./shop/product-details.component').then(m => m.ProductDetailsComponent)
+    },
+
+  
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
